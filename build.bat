595075639af0eacbe22b4f3e740d19c47389c3d3
leidawt <wxh19970605@126.com>
2023-06-20 20:26:05 +0800

    更新打包脚本

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
-@REM NOTE: To reduce package size, we only compile PicoAWG.py and picoawg_driver.py into PicoAWG.exe, the other dependences (e.g. *.png numpy, matplotlib) are not included.
-nuitka --disable-console --follow-import-to=picoawg_driver  --nofollow-imports --output-dir=dist --windows-icon-from-ico=./icon.ico  --remove-output PicoAWG.py
+python -m nuitka --disable-console --standalone  --nofollow-import-to=numpy --enable-plugin=tk-inter --output-dir=dist --windows-icon-from-ico=./icon.ico --include-data-files=./icon.png=./icon.png  --remove-output PicoAWG.py
 
-xcopy /y .\dist\PicoAWG.exe .\
+@REM xcopy /y .\dist\PicoAWG.exe .\
